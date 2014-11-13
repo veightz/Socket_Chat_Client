ac5eaff6f71d95d792719177ab76f4f67e59d513
veightz <veightz@gmail.com>
2014-11-13 14:04:57 +0800

    Initial Commit

---
+++ W_Client/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  W_Client
+//
+//  Created by Veight Zhou on 11/13/14.
+//  Copyright (c) 2014 Veight Zhou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ W_Client/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  W_Client
+//
+//  Created by Veight Zhou on 11/13/14.
+//  Copyright (c) 2014 Veight Zhou. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
